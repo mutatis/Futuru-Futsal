3e83337717acc8e39ffa6423b903e76c2895476f
Gabriel <mutatisguga@gmail.com>
2016-05-06 16:49:51 -0300

    x

---
+++ Assets/Script/Jogador.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jogador : MonoBehaviour
+{
+    bool click;
+
+    float mousex;
+    float mousey;
+
+    void Update()
+    {
+        mousex = Input.mousePosition.x;
+
+        mousey = Input.mousePosition.y;
+
+        if (click)
+        {
+            transform.position = Camera.main.ScreenToWorldPoint(new Vector3(mousex, mousey, 10));
+        }
+    }
+
+    public void OnMouseDown()
+    {
+        click = true;
+    }
+
+    public void OnMouseUp()
+    {
+        click = false;
+    }
+}
---
+++ Assets/Script/Segue.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Segue : MonoBehaviour
+{
+
+    public Transform pos;
+    	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = pos.position;
+	}
+}
